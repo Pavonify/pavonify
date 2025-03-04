39cdf873962f6a514a6472c5cf66ef8dcaee0db2
Pavonify <swillard8@googlemail.com>
2025-03-04 08:54:58 +0530

    Blog Creation

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at")
+    prepopulated_fields = {"slug": ("title",)}  # Auto-fill slug from title
+
+admin.site.register(BlogPost)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
 
-# Create your views here.
+def blog_list(request):
+    """Show all blog posts."""
+    posts = BlogPost.objects.all()
+    return render(request, "blog/blog_list.html", {"posts": posts})
+
+def blog_detail(request, slug):
+    """Show a single blog post."""
+    post = get_object_or_404(BlogPost, slug=slug)
+    return render(request, "blog/blog_detail.html", {"post": post})
